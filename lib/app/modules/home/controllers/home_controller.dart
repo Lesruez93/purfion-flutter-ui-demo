eeb566a0135d2d3f8c6863b32a4316b849be168a
lesruez93 <lesterrusike@gmail.com>
2024-04-11 23:20:43 +0200

    Initial

--- lib/app/modules/home/controllers/home_controller.dart
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -1,13 +1,12 @@
 import 'dart:async';
-import 'dart:ffi';
 import 'dart:math';
-
 import 'package:get/get.dart';
-import 'package:get/get_rx/get_rx.dart';
 
+// Created on 11/04/2023
+// By Lester Rusike
 class HomeController extends GetxController {
   //TODO: Implement HomeController
-  Timer? _timer ;
+  Timer? _timer;
 
   final count = 0.obs;
   RxString timerNumber = '55'.obs;
@@ -23,8 +22,8 @@ class HomeController extends GetxController {
       // Run your code here at the specified interval (1 second in this case)
       int randomNumber = Random().nextInt(99) + 1;
       timerNumber.value = randomNumber.toString();
-      temp.value =  Random().nextInt(40) + 1;
-      humidity.value =  Random().nextInt(40) + 1;
+      temp.value = Random().nextInt(40) + 1;
+      humidity.value = Random().nextInt(40) + 1;
       filterLife.value = Random().nextInt(99) + 1;
     });
     super.onInit();
